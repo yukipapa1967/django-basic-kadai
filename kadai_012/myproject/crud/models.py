a4f97a6c39334e77e0dbcbf26ec70f6ee3a861db
yukipapa1967 <tg.wara0810@gmail.com>
2024-08-19 03:36:44 +0900

    kadai_012 の提出

---
+++ kadai_012/myproject/crud/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.urls import reverse
+
+class Category(models.Model):
+  name = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.name
+
+
+
+class Product(models.Model):
+  name = models.CharField(max_length=200)
+  price = models.PositiveIntegerField()
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+  img = models.ImageField(blank=True, default='noImage.png')
+  comment = models.TextField(max_length=400, blank=True, null=True)  #ここに追加済み
+ 
+  def __str__(self):
+    return self.name
+
+  # 新規作成・編集完了時のリダイレクト先
+  def get_absolute_url(self):
+    return reverse('list')
+
